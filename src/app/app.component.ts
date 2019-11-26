97c807fb2f2f7b5ea53cefe771cc627f92b5a66e
sivakumargavs <sivakumargavs@users.noreply.github.com>
2019-11-26 09:13:58 +0530

    Touched service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {PriceService} from './price.service'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component1.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
+  myForm:FormGroup
+  sku = "test"
+  ps:PriceService
+  taxPrice: number  
+
+  constructor(fb:FormBuilder){  
+    this.ps = new PriceService()
+
+    this.myForm = fb.group({
+      "sku":['adsfsdfsdfC123', Validators.required]
+    })
+  }
+
   title = 'firstApp';
+  selva = "delux"
+  data = {
+    "admin": "true",
+    "salary": 2000,
+    "sex": "Male",
+    "category":"super-delux"
+  }
+  colorRed = "red"
+  cities = ["Chennai", "Madurai", "Coimbatore"]
+  isMale():boolean {
+    return !(this.data.sex == "Male")    
+  };
+
+  getGender():string {
+    if(this.data.sex == "Male"){
+      return "male";
+    }
+    else{
+      return "female";
+    }
+  };
+  ngOnInit(){
+
+  }
+  onTestSumbit(form:any): void{
+    this.taxPrice = this.ps.calculateTax(form.sku)   
+  }
+  onSumbit(price:number): void{    
+    console.log("you have submitted from form builder ", this.taxPrice) 
+   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router'
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { LoadingComponent } from './loading/loading.component';
+
+const routes:Routes = [
+  { path: '', component: LoadingComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about/:category/:product', component: AboutComponent }
+  // { path: 'contact', component: ContactComponent },
+  // { path: 'contactus', redirectTo: 'contact' }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    LoadingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes), // <-- routes
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
