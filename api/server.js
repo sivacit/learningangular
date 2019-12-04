8a8d94c34d82c5ec47d977400f2f6db423678c79
sivakumargavs <sivakumargavs@users.noreply.github.com>
2019-12-04 09:01:46 +0530

    added git service

--- api/server.js
+++ api/server.js
@@ -5,39 +5,55 @@ const express = require('express'),
     mongoose = require('mongoose'),
     config = require('./DB');
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://siva:Siva1982@learning-zvrze.azure.mongodb.net/test?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-// client.connect(err => {
-//     const collection = client.db("test").collection("devices");
-//     // perform actions on the collection object
-//     client.close();
-// });
-const businessRoute = require('./business.route');
+let Device = require('./Device')
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DB, { useNewUrlParser: true }).then(
   () => {console.log('Database is connected') },
   err => { console.log('Cannot connect to the database'+ err)}
 );
-var version=process.env.version || "1.0"
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 // app.use(express.static(path.join(__dirname,'../dist/angular7crud')));
 
-app.get('/getversion',function(req,res){
-  console.log('Version '+version);
-  res.status(200).json({version:version})
-});
-app.use('/business', businessRoute);
+app.post("/devices", function(req, res){
+  let dev = new Device(req.body);
+  dev.save()
+    .then(dev => {
+      res.status(200).json({'Device': 'Device in added successfully'});
+    })
+    .catch(err => {
+    res.status(400).send("unable to save to database");
+    });
+})
 
-// app.use('/',function(req,res){
-//   res.sendFile(path.join(__dirname,'../dist/angular7crud','index.html'))
-// });
+app.get("/devices", function (req, res) {
+  Device.find(function (err, devList){
+  if(err){
+    console.log(err);
+  }
+  else {
+    res.json(devList);
+  }
+});
+});
+
+app.get("/devices/:id", function (req, res) {
+  let id = req.params.id;
+  Device.findById(id, function (err, device){
+    if (err){
+      console.log(err)
+    }
+    else{
+      res.json(device);
+    }    
+  });
+});
+
+
 const port = process.env.PORT || 3000;
 
 const server = app.listen(port, function(){
   console.log('Listening on port ' + port);
-  console.log('Version '+version);
 });
